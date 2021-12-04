22bdfe3e2b69c426109cbb3e488d60daae8cb7af
Dru444 <aman.mehra18@st.niituniversity.in>
2021-12-05 00:10:01 +0530

    final app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "daaama1f11ec6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "daaam": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "songStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
